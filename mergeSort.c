b79bd5279e749a40f49e6827817714b383f1ae8f
Büşra Asan <46824096+busraasan@users.noreply.github.com>
2021-10-29 13:58:27 +0300

    deleted some unnecessary parts

--- mergeSort.c
+++ mergeSort.c
@@ -47,34 +47,3 @@ void merge(int* arr, int p, int q, int r){
         k++;
     }
 }
-
-
-
-int main()
-{
-    int N = parse_int(ltrim(rtrim(readline())));
-
-    char** arr_temp = split_string(rtrim(readline()));
-
-    int* arr = malloc(N * sizeof(int));
-
-    for (int i = 0; i < N; i++) {
-        int arr_item = parse_int(*(arr_temp + i));
-
-        *(arr + i) = arr_item;
-    }
-
-  	mergeSort(arr, 0, N-1);
-  
-    for (int i = 0; i < N; i++) {
-        printf("%d", *(arr + i));
-
-        if (i != N - 1) {
-            printf(" ");
-        }
-    }
-
-    printf("\n");
-
-    return 0;
-}
